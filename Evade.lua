7ed4082b06a77da4c4438b9ead18db2db91825ec
RGV-devid <youhubdoorsmobile@gmail.com>
2025-03-26 11:53:20 +0700

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -1,1 +1,12 @@
+local Library = loadstring(game:HttpGet("https://github.com/RGV-devid/Script/blob/main/Library/MacLib.lua?raw=true"))()
 
+local Window = MacLib:Window({
+	Title = "YOUHUB | EVADE",
+	Subtitle = "v0.0.1",
+	Size = UDim2.fromOffset(758, 500),
+	DragStyle = 1,
+	DisabledWindowControls = {},
+	ShowUserInfo = true,
+	Keybind = Enum.KeyCode.RightControl,
+	AcrylicBlur = true,
+})
